f93d1294b146db981b26483e21377d933b3a3d57
amujka <mujkanovic.alen091@gmail.com>
2020-10-25 19:14:33 +0100

    delete footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieList from "./components/movieList/MovieList";
 import Header from "./components/header/Header";
 // import Footer from './components/footer/Footer'
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import MovieDetails from "./components/movieList/movieDetails/MovieDetails";
+import MovieDetails from "./components/movieList/singleMovieDetails/MovieDetails";
 
 
 function App() {
